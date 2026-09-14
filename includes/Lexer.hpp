f8c46b121c2f337006a3532d93a8399555ab69d8
gmanique <gaetan5483@gmail.com>
2026-09-15 01:12:58 +0200

    feat: Lexer class start

--- includes/Lexer.hpp
+++ includes/Lexer.hpp
@@ -1,27 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   FileReader.hpp                                     :+:      :+:    :+:   */
+/*   Lexer.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmanique <gmanique@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/15 01:09:47 by gmanique          #+#    #+#             */
-/*   Updated: 2026/09/15 01:09:49 by gmanique         ###   ########.fr       */
+/*   Created: 2026/09/15 01:09:43 by gmanique          #+#    #+#             */
+/*   Updated: 2026/09/15 01:09:44 by gmanique         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILEREADER_HPP
-#define FILEREADER_HPP
+#ifndef LEXER_HPP
+#define LEXER_HPP
 
-#include <filesystem>
-#include <optional>
+#include <cstdint>
+#include <list>
 #include <string>
+#include <string_view>
 
-class FileReader {
+typedef enum e_tokenID { EOFF, NUMBER, OPERATOR, SEPARATOR, WORD } t_tokenID;
+
+typedef struct s_token {
+  t_tokenID id;
+  std::string_view value;
+} t_token;
+
+class Lexer {
+private:
+  std::list<t_token> _tokens;
+  std::string_view _file;
+
 public:
-  std::optional<std::string> read_file(const std::filesystem::path &path);
-
-  // private:
+  Lexer(std::string &file);
+  Lexer(std::string_view &file);
+  uint8_t lex_file();
 };
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,19 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmanique <gmanique@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 01:09:05 by gmanique          #+#    #+#             */
+/*   Updated: 2026/09/15 01:09:10 by gmanique         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 // #include "Logging.hpp"
 #include <FileReader.hpp>
 // #include <format>
+#include "Lexer.hpp"
 #include <iostream>
 
 int main(int ac, char **av) {
   if (ac < 2)
-    return (std::cout << "Welcome in the Glang Compiler version 0.0.1 !\n", 0);
+    return (std::cout << "Welcome in the Glang Compiler version 0.1.0 !\n", 0);
   if (ac > 2)
     return (std::cout << "I can only handle one file at once yet.\n", 0);
 
   std::string path(av[1]);
   FileReader a;
   std::optional<std::string> tmp = a.read_file(path);
-  if (tmp.has_value()) {
-    std::cout << tmp.value();
+  if (!tmp.has_value()) {
+    return (std::cout << "Empty or inexistant file", 1);
   }
+
+  std::cout << tmp.value();
+
+  Lexer lex(tmp.value());
+  uint8_t status;
+  if ((status = lex.lex_file())) {
+    return (status);
+  }
+
   return (0);
 }
