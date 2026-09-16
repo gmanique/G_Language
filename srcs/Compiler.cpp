2560e5afaa00976b37c12828d59bde28ff2b4808
gmanique <gaetan5483@gmail.com>
2026-09-16 02:18:02 +0200

    fix: Potential memory issue when lexing multiple big files

--- srcs/Compiler.cpp
+++ srcs/Compiler.cpp
@@ -6,7 +6,7 @@
 /*   By: gmanique <gmanique@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/16 01:54:06 by gmanique          #+#    #+#             */
-/*   Updated: 2026/09/16 02:05:41 by gmanique         ###   ########.fr       */
+/*   Updated: 2026/09/16 02:16:51 by gmanique         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,28 @@
 #include "FileReader.hpp"
 
 bool Compiler::lex_all(const std::vector<std::string> &paths) {
-  for (const auto &path : paths) {
-    SourceFile file;
-    file.path = path;
 
-    FileReader reader;
+  FileReader reader;
+
+  for (const auto &path : paths) {
     auto content = reader.read_file(path);
     if (!content)
       return false;
 
-    file.content = std::move(*content);
+    SourceFile &file = _files[path];
+    file.path = path;
+    file.content = std::move(*content); // Adresse 100% stable
+
     file.lexer = std::make_unique<Lexer>(file.content);
 
     if (file.lexer->lex_file() != 0) {
       return false;
     }
+  }
 
-    // NOTE: Delete when done
-    file.lexer->Print();
-
-    _files[path] = std::move(file);
+  // NOTE: Delete this print when done
+  for (const auto &path : paths) {
+    _files[path].lexer->Print();
   }
   return true;
 }
